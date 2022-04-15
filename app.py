b8625b83280089624ab4e1e572e17c705f65650f
Will <32254701+willseff@users.noreply.github.com>
2022-04-15 19:06:49 -0400

    Update app.py
    web app currently only prints search results

--- app.py
+++ app.py
@@ -25,19 +25,24 @@ def my_form_post():
     driver.get("https://www.google.ca/")
 
     search = driver.find_element(by=By.CLASS_NAME, value="gLFyf")
-    search_string = "\"Location * {}\" \"{}\" site:linkedin.com/in OR site:linkedin.com/pub -intitle:profiles -inurl:\"/dir".format(keywords, location)
+    search_string = "\"Location * {}\" \"{}\" site:linkedin.com/in OR site:linkedin.com/pub -intitle:profiles".format(location, keywords)
     search.send_keys(search_string) 
     search.send_keys(Keys.RETURN)
 
     driver.implicitly_wait(30)
 
-    descriptions = []
+    result = {}
 
     for page in range(10):
-        
+
         list_elements = driver.find_elements_by_class_name("jtfYYd")
-        descs = [descriptions.append(element.find_element_by_class_name("VwiC3b").text) for element in list_elements]
-        descriptions.append(descs)
+
+        for item in list_elements:
+            title = item.find_element_by_class_name("LC20lb").text
+            desc = item.find_element_by_class_name("VwiC3b").text
+            result[title] = desc
+        #descs = [descriptions.append(element.find_element_by_class_name("VwiC3b").text) for element in list_elements]
+        #descriptions.append(descs)
         try:
             link = driver.find_element_by_link_text("Next")
             link.click()
@@ -45,6 +50,6 @@ def my_form_post():
             break
 
 
-    return str(descriptions)
+    return render_template("results.html", result=result)
 
 app.run(debug=True, host='0.0.0.0', port=8000)
