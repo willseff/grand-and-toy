986dc1025bfc5cd8e1e3b5bea07e762c7354d61e
Will <32254701+willseff@users.noreply.github.com>
2022-04-15 15:45:25 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, render_template
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+import time
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    keywords = request.form['keywords']
+
+    location = request.form['location']
+
+    PATH = "/Users/williamli/Documents/Grand and Toy/chromedriver-2"
+    driver = webdriver.Chrome(PATH)
+
+    driver.get("https://www.google.ca/")
+
+    search = driver.find_element(by=By.CLASS_NAME, value="gLFyf")
+    search_string = "\"Location * {}\" \"{}\" site:linkedin.com/in OR site:linkedin.com/pub -intitle:profiles -inurl:\"/dir".format(keywords, location)
+    search.send_keys(search_string) 
+    search.send_keys(Keys.RETURN)
+
+    driver.implicitly_wait(30)
+
+    descriptions = []
+
+    for page in range(10):
+        
+        list_elements = driver.find_elements_by_class_name("jtfYYd")
+        descs = [descriptions.append(element.find_element_by_class_name("VwiC3b").text) for element in list_elements]
+        descriptions.append(descs)
+        try:
+            link = driver.find_element_by_link_text("Next")
+            link.click()
+        except:
+            break
+
+
+    return str(descriptions)
+
+app.run(debug=True, host='0.0.0.0', port=8000)
