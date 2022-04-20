8d66e02dd7009f0c06dc8a018eb9b2a2d3096726
Will <32254701+willseff@users.noreply.github.com>
2022-04-20 11:21:48 -0400

    make it better

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_form():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
     return render_template('index.html')
 
